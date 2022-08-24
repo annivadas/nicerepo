dfdbf9ab8eb50631743642e63e7cd6d087c4ad68
annivadas <anni.vadas@gmail.com>
2022-08-24 19:38:24 +0200

    sikerült középre helyezni

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-/* const handleClick = () => {
-    let elements = document.querySelectorAll(".button");
-    for(let i=0; i<elements.length; i++) {
-        elements[i].addEventListener("click", () => console.log(elements[i].innerHTML)
-
-        )
-    }
-}
-handleClick() */
-
-/* const handleClick = () => {
-    let elements = document.querySelectorAll('.button');
-    for (let i = 0; i < elements.length; i += 1) {
-      elements[i].addEventListener('click', () => console.log(elements[i].innerHTML)
-      )
-    }
-  }
-  handleClick() */
 
 /*   const handleClick = () => {
-    const buttonElements = document.getElementsByClassName("button");
-    for (let i = 0; i < buttonElements.length; i++) {
-      buttonElements[i].addEventListener("click", function () {
-        console.log(buttonElements[i].textContent);
+  const buttons = document.getElementsByClassName("button");
+  for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+        console.log(buttons[i].textContent);
       });
     }
   };
   handleClick() */
 
- const handleClick = () => {
+const handleClick = () => {
   const buttons = document.querySelectorAll(".button");
   buttons.forEach((btn) =>
     btn.addEventListener("click", () => {
@@ -35,7 +17,16 @@ handleClick() */
     })
   );
 };
-handleClick()  
+/* const handleClick = () => {
+    let elements = document.querySelectorAll(".button");
+    for(let i=0; i<elements.length; i++) {
+        elements[i].addEventListener("click", () => console.log(elements[i].innerHTML)
+        )
+    }
+} */
+
+handleClick()
+
 
 /* let elements = document.querySelectorAll('.button');
 for (let i = 0; i < elements.length; i += 1) {
@@ -44,3 +35,4 @@ for (let i = 0; i < elements.length; i += 1) {
   })
 } */
   // jelen esetben 0 vagy 1 vagy 2
+
